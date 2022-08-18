0a97976e84a70e1f74a69f7d3cefcbeb92ae04e2
DesigneR- <limjunyi03@gmail.com>
2022-08-18 13:09:14 +0800

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -30,6 +30,15 @@ const instagramIcon = `<svg class="fa instagram" xmlns="http://www.w3.org/2000/s
 const linkedinIcon = `<svg class="fa linked-in" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
 <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"/>
 </svg>`;
+const locationIcon = `<svg class="fa location" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+<path d="M168.3 499.2C116.1 435 0 279.4 0 192C0 85.96 85.96 0 192 0C298 0 384 85.96 384 192C384 279.4 267 435 215.7 499.2C203.4 514.5 180.6 514.5 168.3 499.2H168.3zM192 256C227.3 256 256 227.3 256 192C256 156.7 227.3 128 192 128C156.7 128 128 156.7 128 192C128 227.3 156.7 256 192 256z"/>
+</svg>`;
+const phoneIcon = `<svg class="fa phone" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+<path d="M511.2 387l-23.25 100.8c-3.266 14.25-15.79 24.22-30.46 24.22C205.2 512 0 306.8 0 54.5c0-14.66 9.969-27.2 24.22-30.45l100.8-23.25C139.7-2.602 154.7 5.018 160.8 18.92l46.52 108.5c5.438 12.78 1.77 27.67-8.98 36.45L144.5 207.1c33.98 69.22 90.26 125.5 159.5 159.5l44.08-53.8c8.688-10.78 23.69-14.51 36.47-8.975l108.5 46.51C506.1 357.2 514.6 372.4 511.2 387z"/>
+</svg>`;
+const emailIcon = `<svg class="fa email" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+<path d="M464 64C490.5 64 512 85.49 512 112C512 127.1 504.9 141.3 492.8 150.4L275.2 313.6C263.8 322.1 248.2 322.1 236.8 313.6L19.2 150.4C7.113 141.3 0 127.1 0 112C0 85.49 21.49 64 48 64H464zM217.6 339.2C240.4 356.3 271.6 356.3 294.4 339.2L512 176V384C512 419.3 483.3 448 448 448H64C28.65 448 0 419.3 0 384V176L217.6 339.2z"/>
+</svg>`;
 
 const rating4starElem = document.getElementById('4-1');
 if (rating4starElem) {
@@ -44,4 +53,19 @@ if (rating4starhalfElem) {
 const footerIconElem = document.getElementById('contact-icon');
 if (footerIconElem) {
     footerIconElem.innerHTML = facebookIcon + twitterIcon + instagramIcon + linkedinIcon;
+}
+
+const locationIconElem = document.getElementById('location-icon');
+if (locationIconElem) {
+    locationIconElem.innerHTML = locationIcon;
+}
+
+const phoneIconElem = document.getElementById('phone-icon');
+if (phoneIconElem) {
+    phoneIconElem.innerHTML = phoneIcon;
+}
+
+const emailIconElem = document.getElementById('email-icon');
+if (emailIconElem) {
+    emailIconElem.innerHTML = emailIcon;
 }
