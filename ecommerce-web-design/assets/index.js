363956916c4b5a24d4fd1a60dba5bcb36d8cf388
DesigneR- <limjunyi03@gmail.com>
2022-08-25 22:42:02 +0800

    Update index.js

--- ecommerce-web-design/assets/index.js
+++ ecommerce-web-design/assets/index.js
@@ -11,3 +11,14 @@ if (menuElem) {
 if (menuCloseElem) {
     menuCloseElem.addEventListener('click', () => navElem.classList.remove('active'));
 }
+
+const mainImage = document.getElementById('main-image');
+const smallImage = document.getElementsByClassName('small-image');
+
+// Change Product Main Image
+if (smallImage) {
+    smallImage[0].addEventListener('click', () => mainImage.src = smallImage[0].src);
+    smallImage[1].addEventListener('click', () => mainImage.src = smallImage[1].src);
+    smallImage[2].addEventListener('click', () => mainImage.src = smallImage[2].src);
+    smallImage[3].addEventListener('click', () => mainImage.src = smallImage[3].src);
+}
