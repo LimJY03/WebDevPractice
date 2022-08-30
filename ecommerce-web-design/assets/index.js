e61ea477b1cf6ba71e06a563bca37129e41c07da
DesigneR- <limjunyi03@gmail.com>
2022-08-30 13:01:24 +0800

    Update index.js

--- ecommerce-web-design/assets/index.js
+++ ecommerce-web-design/assets/index.js
@@ -12,13 +12,32 @@ if (menuCloseElem) {
     menuCloseElem.addEventListener('click', () => navElem.classList.remove('active'));
 }
 
-const mainImage = document.getElementById('main-image');
-const smallImage = document.getElementsByClassName('small-image');
-
 // Change Product Main Image
-if (smallImage) {
-    smallImage[0].addEventListener('click', () => mainImage.src = smallImage[0].src);
-    smallImage[1].addEventListener('click', () => mainImage.src = smallImage[1].src);
-    smallImage[2].addEventListener('click', () => mainImage.src = smallImage[2].src);
-    smallImage[3].addEventListener('click', () => mainImage.src = smallImage[3].src);
+const inProductPage = document.getElementById('product-details');
+
+if (inProductPage) {
+
+    const mainImage = document.getElementById('main-image');
+    const smallImage = document.getElementsByClassName('small-image');
+    
+    [...smallImage].forEach((elem, i) => elem.addEventListener('click', () => mainImage.src = smallImage[i].src))
+}
+
+// Calculate Cart Total
+const inCartPage = document.getElementById('cart');
+
+if (inCartPage) {
+
+    const productCount = document.getElementsByClassName('product-count');
+    const productTotal = document.getElementsByClassName('product-total');
+    const productSubtotal = document.getElementById('cart-subtotal');
+    const productFinalTotal = document.getElementById('final-total');
+
+    function update(id) {
+        productTotal[id].innerHTML = `$${118.19 * productCount[id].value}`;
+        productSubtotal.innerHTML = `$${[...productTotal].map(elem => parseFloat(elem.innerHTML.slice(1))).reduce((a, b) => a + b).toFixed(2)}`;
+        productFinalTotal.childNodes[0].innerHTML = productSubtotal.innerHTML;
+    }
+
+    [...productCount].forEach((elem, i) => elem.addEventListener('change', () => update(i)))
 }
