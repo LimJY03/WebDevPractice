24e396bb598805e56b04f3f520b06a8eec2ca510
DesigneR- <limjunyi03@gmail.com>
2022-09-05 22:08:10 +0800

    Added Newsletter Component

--- restaurant-web-design/src/components/Footer/Newsletter.jsx
+++ restaurant-web-design/src/components/Footer/Newsletter.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+
 import './Newsletter.css';
 
-const Newsletter = () => (
-  <div>
-    Newsletter
-  </div>
-);
+const Newsletter = () => {
+	return (
+		<div className='app__newsletter'>
+			<div className='app__newsletter-heading'>
+				<SubHeading title='Newslettter' />
+				<h1 className='headtext__cormorant'>Subscribe To Our Newsletter</h1>
+				<p className='p__opensans'>And never miss latest Updates!</p>
+			</div>
+			<div className='app__newsletter-input flex__center'>
+				<input type='email' placeholder='Enter your mail address' />
+				<button className='custom__button'>Subscribe</button>
+			</div>
+		</div>
+	);
+}
 
 export default Newsletter;
